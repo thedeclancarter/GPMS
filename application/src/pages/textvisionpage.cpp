50fb553c251ceb0b4f9465435960fc6877f5ed35
Victoria <102815017+vimoreno21@users.noreply.github.com>
2024-11-04 16:47:57 +0000

    App/api (#27)
    * Connected Test API
    
    * Improved UI with API Images
    
    * Bug-fix: Stop Timer When Clicking Logo
    
    * Connected Test API
    
    * Improved UI with API Images
    
    * Bug-fix: Stop Timer When Clicking Logo
    
    * Added Functions to Clear Inputs and Called When Logo Clicked
    
    * Disabled Calibration Button if not Calibrated
    
    * Reset the Disabled Feature
    
    * fixing some merge conflicts
    
    * Refresh images when rejecting on all options
    
    * And logo

--- application/src/pages/textvisionpage.cpp
+++ application/src/pages/textvisionpage.cpp
@@ -58,6 +58,10 @@ void TextVisionPage::clearInput(){
     m_visionInput->clear();
 }
 
+void TextVisionPage::clearInput(){
+    m_visionInput->clear();
+}
+
 bool TextVisionPage::eventFilter(QObject *obj, QEvent *event)
 {
     if (obj == m_visionInput && isRunningOnRaspberryPi()) {
