fe0a4c8e00381ccde34252c4bb247119ccda67ff
vimoreno21 <102815017+vimoreno21@users.noreply.github.com>
2024-11-10 11:51:10 -0500

    Made screen shorter

--- application/src/main.cpp
+++ application/src/main.cpp
@@ -6,14 +6,14 @@
 
 int main(int argc, char *argv[])
 {
-    // for rasp pi
-    #ifdef Q_OS_RASPBERRYPI
-        qputenv("QT_QPA_PLATFORM", QByteArray("eglfs"));
-    #endif
+// for rasp pi
+#ifdef Q_OS_RASPBERRYPI
+    qputenv("QT_QPA_PLATFORM", QByteArray("eglfs"));
+#endif
 
     QApplication a(argc, argv);
     MainWindow w;
-    w.setFixedSize(1024, 600);
+    w.setFixedSize(1024, 580);
     w.show();
     return a.exec();
 }
